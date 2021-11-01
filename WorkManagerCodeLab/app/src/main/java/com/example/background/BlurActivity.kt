b1c4225093845daf4f46a199be715b0fa22c3201
avelycure <van.emelin2012@yandex.ru>
2021-11-01 17:42:35 +0300

    Show final output

--- WorkManagerCodeLab/app/src/main/java/com/example/background/BlurActivity.kt
+++ WorkManagerCodeLab/app/src/main/java/com/example/background/BlurActivity.kt
@@ -16,6 +16,7 @@
 
 package com.example.background
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
@@ -41,6 +42,15 @@ class BlurActivity : AppCompatActivity() {
         binding.goButton.setOnClickListener { viewModel.applyBlur(blurLevel) }
 
         viewModel.outputWorkInfos.observe(this, workInfosObserver())
+
+        binding.seeFileButton.setOnClickListener {
+            viewModel.outputUri?.let { currentUri ->
+                val actionView = Intent(Intent.ACTION_VIEW, currentUri)
+                actionView.resolveActivity(packageManager)?.run {
+                    startActivity(actionView)
+                }
+            }
+        }
     }
 
     private fun workInfosObserver(): Observer<List<WorkInfo>> {
@@ -59,11 +69,22 @@ class BlurActivity : AppCompatActivity() {
             // Every continuation has only one worker tagged TAG_OUTPUT
             val workInfo = listOfWorkInfo[0]
 
-            if (workInfo.state.isFinished) {
-                showWorkFinished()
-            } else {
-                showWorkInProgress()
-            }
+            if (workInfo.state.isFinished)
+                if (workInfo.state.isFinished) {
+                    showWorkFinished()
+
+                    // Normally this processing, which is not directly related to drawing views on
+                    // screen would be in the ViewModel. For simplicity we are keeping it here.
+                    val outputImageUri = workInfo.outputData.getString(KEY_IMAGE_URI)
+
+                    // If there is an output file show "See File" button
+                    if (!outputImageUri.isNullOrEmpty()) {
+                        viewModel.setOutputUri(outputImageUri)
+                        binding.seeFileButton.visibility = View.VISIBLE
+                    }
+                } else {
+                    showWorkInProgress()
+                }
         }
     }
 
