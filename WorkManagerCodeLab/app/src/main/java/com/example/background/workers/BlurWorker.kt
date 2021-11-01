ddfa64daf034d6e7447f8fb36f2102bac3039ce0
avelycure <van.emelin2012@yandex.ru>
2021-11-01 17:02:53 +0300

    Chained work

--- WorkManagerCodeLab/app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ WorkManagerCodeLab/app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -21,27 +21,29 @@ class BlurWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
 
         makeStatusNotification("Blurring image", appContext)
 
+        sleep()
+
         return try {
             if (TextUtils.isEmpty(resourceUri)) {
-                Log.e(TAG, "Invalid input uri")
                 throw IllegalArgumentException("Invalid input uri")
             }
+
             val resolver = appContext.contentResolver
 
             val picture = BitmapFactory.decodeStream(
-                resolver.openInputStream(Uri.parse(resourceUri)))
+                resolver.openInputStream(Uri.parse(resourceUri))
+            )
 
             val output = blurBitmap(picture, appContext)
 
-            val outputUri = writeBitmapToFile(appContext, picture)
-
-            makeStatusNotification("Output is $outputUri", appContext)
+            // Write bitmap to a temp file
+            val outputUri = writeBitmapToFile(appContext, output)
 
             val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
 
             Result.success(outputData)
         } catch (throwable: Throwable) {
-            Log.e(TAG, "Error applying blur")
+            throwable.printStackTrace()
             Result.failure()
         }
     }
